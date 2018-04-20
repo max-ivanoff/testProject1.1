f3b013fa3584c80975ea09e8f073b365bfa43adf
max-ivanoff <max.n.ivanov@mail.ru>
2018-04-20 17:21:16 +0300

    test1 add assertEquals

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -17,7 +17,7 @@ public class FirstTest {
         System.setProperty("webdriver.chrome.driver", "D:\\AutoQA\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://blazedemo.com");
-        WebElement FindFlights =  driver.findElement(By.cssSelector("input.btn.btn-primary")); //input.btn.btn-primary //div.container
+        WebElement FindFlights =  driver.findElement(By.cssSelector("input.btn.btn-primary"));
         FindFlights.click();
 //      assertTrue(driver.findElement(By.cssSelector("body")).isDisplayed());
         assertEquals("http://blazedemo.com/reserve.php", driver.getCurrentUrl());
